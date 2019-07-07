17c7c371755df7b0b57cf28af3933337f2b932ae
Haider Khan <hkhan@datalofts.com>
2019-07-07 16:08:21 -0700

    Updated to work on MacOS

--- ParsingKit/ParsingKit.h
+++ ParsingKit/ParsingKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 zkhaider. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for ParsingKit.
 FOUNDATION_EXPORT double ParsingKitVersionNumber;
